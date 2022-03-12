74043624d6da57da59efc6c5e987f55cc11a3dfb
Marcio Tedesco <martedesco@github.com>
2022-03-12 20:00:10 +0100

    Add Octokit plus test

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/action");
 
 try {
+
+const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+// See https://developer.github.com/v3/issues/#create-an-issue
+const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+  owner,
+  repo,
+  title: "My test issue",
+});
+console.log("Issue created: %s", data.html_url);
+
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
